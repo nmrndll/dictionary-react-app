9be5f072e858b53e297d1066c119a1e8d0454ffb
nmrndll <81620556+nmrndll@users.noreply.github.com>
2021-10-26 13:39:35 +0800

    css update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+          </main>
+        <footer className="text-center">Coded by Naomi Randall</footer>
+      </div>
     </div>
+  
   );
 }
 
-export default App;
